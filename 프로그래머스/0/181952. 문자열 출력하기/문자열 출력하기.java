5bb50c333e88eae42e88aec2129335de03401b84
Kim yeonhee <65239053+yeonhee314@users.noreply.github.com>
2024-09-24 15:33:48 +0900

    [level 0] Title: 문자열 출력하기, Time: 196.16 ms, Memory: 62.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+    // 백준허브 테스트용
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
