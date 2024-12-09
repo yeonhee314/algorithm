1a3717b5b72e3e404192ed0b55996d073f33848b
Kim yeonhee <mouse97_@naver.com>
2024-12-09 17:22:53 +0900

    [level 0] Title: 두 수의 합, Time: 460.32 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,8 @@
+import java.math.BigInteger;
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger b1 = new BigInteger(a);
+    	BigInteger b2 = new BigInteger(b);
+        return String.valueOf(b1.add(b2));
+    }
+}
