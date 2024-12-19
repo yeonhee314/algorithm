0b3706103047e3ddadd990fd5c2d31f2f5f8eccc
Kim yeonhee <mouse97_@naver.com>
2024-12-19 15:21:23 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 0;
+        if(a == b && b == c && c == a) {
+        	answer = (a + b + c) * (int)(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) * (int)(Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3));        	
+        }else if(a != b && b != c && c != a) {
+        	answer = a + b + c;
+        }else {
+        	answer = (a + b + c) * (int)(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+        }
+        
+        return answer;
+    }
+}
