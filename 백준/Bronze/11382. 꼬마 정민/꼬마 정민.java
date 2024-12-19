58f2f4de8cf8aa2f6aac356cfe8de473d3c4c3c4
Kim yeonhee <mouse97_@naver.com>
2024-12-19 10:13:27 +0900

    [Bronze V] Title: 꼬마 정민, Time: 176 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;    
+class Main{
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+		long a, b, c;
+		a = sc.nextLong();
+		b = sc.nextLong();
+		c = sc.nextLong();
+		System.out.println(a + b + c);
+	}
+}
