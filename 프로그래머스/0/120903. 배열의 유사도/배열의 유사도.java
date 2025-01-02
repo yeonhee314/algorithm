337a2832b6624ada85cc55af65fe19f5bdf68736
Kim yeonhee <mouse97_@naver.com>
2025-01-03 01:22:58 +0900

    [level 0] Title: 배열의 유사도, Time: 0.23 ms, Memory: 94.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] s1, String[] s2) {
+               int answer = 0;
+        for(int i = 0; i < s2.length; i++){
+            for(int j = 0; j < s1.length; j++){
+                if(s2[i].equals(s1[j])){
+                    answer++;
+                    System.out.println(s2[i]);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
