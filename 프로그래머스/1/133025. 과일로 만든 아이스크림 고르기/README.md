ece390788ce30a88ac78ca8c61a616a6b2ebad39
kim yeonhee <mouse97_@naver.com>
2025-10-11 17:30:04 +0900

    [level 1] Title: 과일로 만든 아이스크림 고르기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/README.md
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 과일로 만든 아이스크림 고르기 - 133025 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133025) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133025?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 10월 01일 17:43:32
+2025년 10월 11일 17:29:57
 
 ### 문제 설명
 
--- 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
+++ 프로그래머스/1/133025. 과일로 만든 아이스크림 고르기/과일로 만든 아이스크림 고르기.sql
@@ -1,3 +1,5 @@
-SELECT I.FLAVOR
-FROM FIRST_HALF F, ICECREAM_INFO I
-WHERE I.FLAVOR = F.FLAVOR AND F.TOTAL_ORDER > 3000 AND I.INGREDIENT_TYPE = 'fruit_based';
+SELECT h.flavor
+FROM FIRST_HALF as h inner join ICECREAM_INFO as i 
+ON h.flavor = i.flavor
+where h.total_order > 3000 AND INGREDIENT_TYPE = 'fruit_based'
+order by h.total_order desc
